a9a1490eb80a5e07998bd9472186bd30a55e1005
hthgiang000 <hthgiang096@gmail.com>
2021-03-29 20:25:46 +0700

    v1

---
+++ responsive.js
@@ -0,0 +1,13 @@
+$(function () {
+    const slider = [
+        {
+            image: "https://cdn.shopify.com/s/files/1/0130/5041/3114/files/demo22_03_x1024.jpg?v=1610964957",
+        },
+        {
+            image: "https://cdn.shopify.com/s/files/1/0130/5041/3114/files/demo22_01_x1024.jpg?v=1610964956",
+        },
+        {
+            image: "https://cdn.shopify.com/s/files/1/0130/5041/3114/files/demo22_02_x1024.jpg?v=1610964956",
+        }
+    ]
+});
